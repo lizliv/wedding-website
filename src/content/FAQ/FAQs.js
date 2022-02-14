78a1c50f37a4a009793e1f0ebfcec1539b569b60
lizliv <elizabethrenee.livingston@gmail.com>
2022-02-14 16:20:28 -0500

    FAQ update
    Still need to get portuguese translation

--- src/content/FAQ/FAQs.js
+++ src/content/FAQ/FAQs.js
@@ -34,6 +34,26 @@ export default {
                 </>
             ),
         },
+        {
+            Question: () => <>Sunday? Why?</>,
+            Answer: () => (
+                <>
+                    As most of you know, Liz is Jewish. We will be having a traditional Jewish wedding officiated by a reconstructionist Rabbi. Jewish weddings cannot happen from sunset Friday night to sunset Saturday because it is Shabbat (the day of rest). We know this makes travel more difficult, but we hope you'll be able to join us!
+                </>
+            ),
+        },
+        {
+            Question: () => <>Jewish wedding, what does that mean?</>,
+            Answer: () => (
+                <>
+                    <p>There are a few traditional Jewish elements that we plan to incorporate in our ceremony. First, there is a wedding contract called a <b>Ketubah</b>, this is signed before the ceremony begins and outlines the grooms responsibilities to the bride. The <b>Bedeken</b> also occurs before the ceremony where the groom lifts the veil to see the brides face. This tradition stems from the Bible story where Jacob was tricked into marrying Leah, the sister of the woman he loved, Rachel.</p>
+                    
+                    <p>There are a few other unique traditions, but for now, we will skip to the end of the ceremony where the groom <b>breaks a glass</b>. This tradition has a couple of different symbolic meanings, but mostly we just want to stomp on a glass!</p>
+
+                    <p>Finally, during the reception there is a celebratory dance called the <b>hora</b>. During this dance, the bride and groom are lifted onto chairs while holding a cloth such as a napkin in between them. There are many aspects to a Jewish wedding that were not listed here, we will not include all traditional aspects, but you can read a nice summary <a href="https://www.brides.com/jewish-wedding-traditions-4783360" target="_blank">here</a></p>
+                </>
+            ),
+        },
     ],
     [LANGUAGE.PT]: [
         {
@@ -65,6 +85,26 @@ export default {
             Answer: () => (
                 <>
                     Junho &eacute; ver&atilde;o nos Estados Unidos. Em Chicago, as temperaturas giram em torno de 26ºC durante o dia e 18ºC durante a noite!
+                </>
+            ),
+        },
+        {
+            Question: () => <>Domingo? Por quê?</>,
+            Answer: () => (
+                <>
+                    As most of you know, Liz is Jewish. We will be having a traditional Jewish wedding officiated by a reconstructionist Rabbi. Jewish weddings cannot happen from sunset Friday night to sunset Saturday because it is Shabbat (the day of rest). We know this makes travel more difficult, but we hope you'll be able to join us!
+                </>
+            ),
+        },
+        {
+            Question: () => <>Casamento judaico, o que isso significa?</>,
+            Answer: () => (
+                <>
+                    <p>There are a few traditional Jewish elements that we plan to incorporate in our ceremony. First, there is a wedding contract called a <b>Ketubah</b>, this is signed before the ceremony begins and outlines the grooms responsibilities to the bride. The <b>Bedeken</b> also occurs before the ceremony where the groom lifts the veil to see the brides face. This tradition stems from the Bible story where Jacob was tricked into marrying Leah, the sister of the woman he loved, Rachel.</p>
+                    
+                    <p>There are a few other unique traditions, but for now, we will skip to the end of the ceremony where the groom <b>breaks a glass</b>. This tradition has a couple of different symbolic meanings, but mostly we just want to stomp on a glass!</p>
+
+                    <p>Finally, during the reception there is a celebratory dance called the <b>hora</b>. During this dance, the bride and groom are lifted onto chairs while holding a cloth such as a napkin in between them. There are many aspects to a Jewish wedding that were not listed here, we will not include all traditional aspects, but you can read a nice summary <a href="https://www.brides.com/jewish-wedding-traditions-4783360" target="_blank">here</a></p>
                 </>
             ),
         },
