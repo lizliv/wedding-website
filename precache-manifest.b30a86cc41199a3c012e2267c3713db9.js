547c0a070365e130f2516350eea21463c17c5da7
lizliv <elizabethrenee.livingston@gmail.com>
2022-02-14 20:04:23 -0500

    Updates

---
+++ precache-manifest.b30a86cc41199a3c012e2267c3713db9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476ebc8cc2de9f9795eb8ccd24dcf808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "905b152b29a26e27fae7",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "cfd1b84f6a9c079feb42",
+    "url": "/static/css/main.eec754db.chunk.css"
+  },
+  {
+    "revision": "905b152b29a26e27fae7",
+    "url": "/static/js/2.5d5f4db7.chunk.js"
+  },
+  {
+    "revision": "cb10f9aba3abc075ad33a6c6559f6928",
+    "url": "/static/js/2.5d5f4db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd1b84f6a9c079feb42",
+    "url": "/static/js/main.7a63bc62.chunk.js"
+  },
+  {
+    "revision": "2ca3a21b073142048ccc",
+    "url": "/static/js/runtime-main.a4c40521.js"
+  },
+  {
+    "revision": "38ba9080a9da64e0568ce5b8119fed3a",
+    "url": "/static/media/airplane.38ba9080.jpg"
+  },
+  {
+    "revision": "8766bc371a5f554733fb62ca8a998b12",
+    "url": "/static/media/faq.8766bc37.jpg"
+  },
+  {
+    "revision": "5fc4f965a31ac2e3db99ac0e04f5f874",
+    "url": "/static/media/header_large.5fc4f965.jpg"
+  },
+  {
+    "revision": "af0b0240adc1b2513568bb212dbc7a44",
+    "url": "/static/media/header_medium.af0b0240.jpg"
+  },
+  {
+    "revision": "a3081ddf3c9d058a9055986b00b67f45",
+    "url": "/static/media/header_small.a3081ddf.jpg"
+  },
+  {
+    "revision": "c6ff88b0928c262318b66ce0ea5e999e",
+    "url": "/static/media/registry.c6ff88b0.jpg"
+  },
+  {
+    "revision": "5d3f232806ab739ba8940beb91af28b7",
+    "url": "/static/media/rsvp.5d3f2328.jpg"
+  },
+  {
+    "revision": "b919db3fd4bc8c5bbadbcf62fc0880a4",
+    "url": "/static/media/schedule.b919db3f.jpg"
+  },
+  {
+    "revision": "9b7433dfe65be5d58fee24e2a3f167d7",
+    "url": "/static/media/thingstodo.9b7433df.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.38553ca9684dd4ed5a4898a5c69df340.js"
+  "/precache-manifest.b30a86cc41199a3c012e2267c3713db9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
