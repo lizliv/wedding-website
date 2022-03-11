bab28beadc1b327199d6c8c5f36171ec2c0fd5d4
lizliv <elizabethrenee.livingston@gmail.com>
2022-03-10 20:31:48 -0500

    Removed API keys

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -13,15 +13,15 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmxHPISOdSZsCgWDgVwzc8s3i9spa_-D0",
-    authDomain: "lizandchristian2022-f3185.firebaseapp.com",
-    databaseURL: "https://lizandchristian2022-f3185-default-rtdb.firebaseio.com",
-    projectId: "lizandchristian2022-f3185",
-    storageBucket: "lizandchristian2022-f3185.appspot.com",
-    messagingSenderId: "71163325861",
-    appId: "1:71163325861:web:ec80894eb1b6950528cc4c",
-    measurementId: "G-KHB2WNH3R0"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
